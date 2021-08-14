417029c1fe74af7a270509dd2d4235be65f95bd8
Viktor Kovacs <crusader8@gmail.com>
2021-08-14 15:21:32 +0200

    make charts more readable

--- src/main/java/hu/crs/coalltzconjecture/Main.java
+++ src/main/java/hu/crs/coalltzconjecture/Main.java
@@ -6,6 +6,7 @@ import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.XYSeries;
 import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,9 +15,9 @@ public class Main {
     public static void main(String[] args) {
 
         // Create Chart
-        XYChart chart = new XYChartBuilder().width(600).height(500).title("Gaussian Blobs").xAxisTitle("X").yAxisTitle("Y").build();
+        XYChart chart = new XYChartBuilder().width(1920).height(1080).title("Gaussian Blobs").xAxisTitle("X").yAxisTitle("Y").build();
 
-// Customize Chart
+        // Customize Chart
         chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
         chart.getStyler().setChartTitleVisible(false);
         chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSW);
@@ -25,9 +26,10 @@ public class Main {
         var sequenceGenerator = new SequenceGenerator();
         sequenceGenerator.sequences(IntStream.range(1, 20).boxed().collect(Collectors.toList()))
                 .forEach(s -> {
-                    chart.addSeries(String.format("Series: %d", s.ys().get(0)), s.xs(), s.ys());
+                    var xySeries = chart.addSeries(String.format("Series: %d", s.ys().get(0)), s.xs(), s.ys());
+                    xySeries.setMarker(SeriesMarkers.NONE);
                 });
 
-        new SwingWrapper(chart).displayChart();
+        new SwingWrapper<>(chart).displayChart();
     }
 }
